c2b6932b53b8ec2c0420f4ab3bc23137b2fad13c
GustavoLambertyCarranza <168040500+GustavoLambertyCarranza@users.noreply.github.com>
2024-06-19 19:29:17 -0300

    Update romans.py 4.0

--- 01/romans.py
+++ 01/romans.py
@@ -1,31 +1,33 @@
 def int_to_roman(num):
-    Milhar = ["", "M", "MM", "MMM"]
-    Centena = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
-    Dezena = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
-    Unidade = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    roman = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
+    val = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
 
-    romano_id = Milhar[num // 1000] + Centena[(num % 1000) // 100] + \
-                Dezena[(num % 100) // 10] + Unidade[num % 10]
+    resultado = ''
 
-    return romano_id
+    for i in range(len(val) - 1, -1, -1):
+        while num >= val[i]:
+            resultado += roman[i]
+            num -= val[i]
 
-def roman_to_int(roman):
-    nums = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    return resultado
 
-    roman = roman.upper()
-    total = 0
-    for i in range(len(roman)):
-        try:
-            value = nums[roman[i]]
-            if i + 1 < len(roman) and nums[roman[i + 1]] > value:
-                total -= value
-            else:
-                total += value
-        except KeyError:
-            raise ValueError('Input is not a valid Roman numeral: %s' % roman)
+print(int_to_roman(2021))
 
-    return total
+pass
 
-print(int_to_roman(3923))
-print(roman_to_int('V'))
+def roman_to_int(s):
+    num = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, "XC": 90, "C": 100, "CD": 400, "D": 500, "CM": 900, "M": 1000}
 
+    resultado = 0
+
+    for i in range(len(s)):
+        if i > 0 and num[s[i]] > num[s[i - 1]]:
+            resultado += num[s[i]] - 2 * num[s[i - 1]]
+        else:
+            resultado += num[s[i]]
+
+    return resultado
+
+print(roman_to_int("DDMXVI"))
+
+pass
